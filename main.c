5dbcd8312da80b0b6033bc249406e63bd94fdaf7
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-04-21 17:46:59 -0300

    18 exercicio

--- main.c
+++ main.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-int buscarElemento(int arr[], int tamanho, int elemento) {
-    if (tamanho == 0)
+int comprimento(char *str) {
+    if (*str == '\0')
         return 0;
-    if (arr[0] == elemento)
-        return 1;
-    return buscarElemento(arr + 1, tamanho - 1, elemento);
+    return 1 + comprimento(str + 1);
 }
 
 int main() {
-    int numeros[] = {2, 4, 6, 8};
-    int tamanho = sizeof(numeros) / sizeof(numeros[0]);
-
-    printf("%d\n", buscarElemento(numeros, tamanho, 6));  // Saída: 1
-    printf("%d\n", buscarElemento(numeros, tamanho, 5));  // Saída: 0
-
+    char palavra[] = "recursivo";
+    printf("%d\n", comprimento(palavra));  // Saída: 9
     return 0;
 }
