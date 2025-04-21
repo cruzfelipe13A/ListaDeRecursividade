1413a6f2dbfe798d106c27ad058c4fa0395b56d7
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-04-21 17:59:03 -0300

    27 exercicio

--- main.c
+++ main.c
@@ -1,24 +1,46 @@
 #include <stdio.h>
 
-int exponenciacaoRapida(int x, int n) {
-    if (n == 0) {
+int encontrarCaminho(int matriz[3][3], int x1, int y1, int x2, int y2) {
+    if (x1 > x2 || y1 > y2 || matriz[x1][y1] == 1) {
+        return 0;
+    }
+    if (x1 == x2 && y1 == y2) {
+        printf("(%d,%d) ", x1, y1);
         return 1;
     }
-    if (n % 2 == 0) {
-        int half = exponenciacaoRapida(x, n / 2);
-        return half * half;
-    } else {
-        return x * exponenciacaoRapida(x, n - 1);
+    printf("(%d,%d) ", x1, y1);
+    if (encontrarCaminho(matriz, x1, y1 + 1, x2, y2)) {
+        return 1;
     }
+    if (encontrarCaminho(matriz, x1 + 1, y1, x2, y2)) {
+        return 1;
+    }
+    return 0;
+}
+
+int contarCaminhos(int matriz[3][3], int x1, int y1, int x2, int y2) {
+    if (x1 > x2 || y1 > y2 || matriz[x1][y1] == 1) {
+        return 0;
+    }
+    if (x1 == x2 && y1 == y2) {
+        return 1;
+    }
+    return contarCaminhos(matriz, x1, y1 + 1, x2, y2) + contarCaminhos(matriz, x1 + 1, y1, x2, y2);
 }
 
 int main() {
-    int x = 3;
-    int n = 4;
+    int matriz[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
 
-    int resultado = exponenciacaoRapida(x, n);
+    printf("Caminho de (0,0) a (2,2): ");
+    encontrarCaminho(matriz, 0, 0, 2, 2);
+    printf("\n");
 
-    printf("Resultado de %d^%d = %d\n", x, n, resultado);
+    int totalCaminhos = contarCaminhos(matriz, 0, 0, 2, 2);
+    printf("Total de caminhos possíveis: %d\n", totalCaminhos);
 
     return 0;
 }
