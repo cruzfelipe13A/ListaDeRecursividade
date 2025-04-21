2e7ebd1701d1cccffd64407ca5126ba963d4694c
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-04-21 17:17:49 -0300

    nono exercicios

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 
-int verificar_palindromo(char *str, int inicio, int fim) {
-    if (inicio >= fim) return 1;
-    if (str[inicio] != str[fim]) return 0;
-    return verificar_palindromo(str, inicio + 1, fim - 1);
+int mdc(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return mdc(b, a % b);
 }
 
 int main() {
-    char str[] = "arara";
-    int comprimento = strlen(str);
-    int resultado = verificar_palindromo(str, 0, comprimento - 1);
-    printf("%d\n", resultado);
+    int a = 48, b = 18;
+    printf("MDC de %d e %d é: %d\n", a, b, mdc(a, b));
     return 0;
 }
