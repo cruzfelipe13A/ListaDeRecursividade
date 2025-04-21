318e594a98c183c194f290cfe71718fac7938f03
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-04-21 17:54:22 -0300

    24 exercicio

--- main.c
+++ main.c
@@ -1,52 +1,39 @@
 #include <stdio.h>
 
-void merge(int arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    int leftArr[n1], rightArr[n2];
-
-    for (int i = 0; i < n1; i++) {
-        leftArr[i] = arr[left + i];
-    }
-    for (int i = 0; i < n2; i++) {
-        rightArr[i] = arr[mid + 1 + i];
-    }
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (leftArr[i] <= rightArr[j]) {
-            arr[k++] = leftArr[i++];
-        } else {
-            arr[k++] = rightArr[j++];
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+    
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
 
-    while (i < n1) {
-        arr[k++] = leftArr[i++];
-    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
 
-    while (j < n2) {
-        arr[k++] = rightArr[j++];
-    }
+    return i + 1;
 }
 
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
 
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
 int main() {
-    int arr[] = {5, 3, 8, 1};
+    int arr[] = {9, 2, 5, 1, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    mergeSort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1);
 
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
