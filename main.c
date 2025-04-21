724d78245306b657b805e1bcc800370dd2d27eff
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-04-21 17:51:03 -0300

    21 exercicio

--- main.c
+++ main.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
-#include <string.h>
 
-void trocar(char *x, char *y) {
-    char temp = *x;
-    *x = *y;
-    *y = temp;
+void imprimirCombinacao(int comb[], int k) {
+    for (int i = 0; i < k; i++) {
+        printf("%d ", comb[i]);
+    }
+    printf("\n");
 }
 
-void permutar(char *str, int inicio, int fim) {
-    if (inicio == fim) {
-        printf("%s\n", str);
-    } else {
-        for (int i = inicio; i <= fim; i++) {
-            trocar(&str[inicio], &str[i]);
-            permutar(str, inicio + 1, fim);
-            trocar(&str[inicio], &str[i]); // backtrack
-        }
+void combinar(int arr[], int n, int k, int index, int comb[], int i) {
+    if (index == k) {
+        imprimirCombinacao(comb, k);
+        return;
     }
+    if (i >= n) return;
+
+    comb[index] = arr[i];
+    combinar(arr, n, k, index + 1, comb, i + 1); // inclui arr[i]
+    combinar(arr, n, k, index, comb, i + 1);     // não inclui arr[i]
 }
 
 int main() {
-    char str[] = "abc";
-    permutar(str, 0, strlen(str) - 1);
+    int arr[] = {1, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+    int comb[10]; // tamanho máximo arbitrário
+
+    combinar(arr, n, k, 0, comb, 0);
     return 0;
 }
